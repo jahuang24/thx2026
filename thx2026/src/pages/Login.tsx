03b11b50c3d5e15213b262e07b37485ddb78587e
jahuang24 <126387182+jahuang24@users.noreply.github.com>
2026-02-07 09:44:51 -0500

    updated login again

--- thx2026/src/pages/Login.tsx
+++ thx2026/src/pages/Login.tsx
@@ -20,19 +20,20 @@ export function Login() {
       <div className="w-full border-t border-white/40 bg-white/30 py-14 backdrop-blur-sm backdrop-saturate-150">
         <div className="flex flex-col items-center justify-center gap-20 px-6 sm:flex-row">
 
-          <button
-            onClick={() => navigate('/doctor-login')}
-            className="min-w-[240px] rounded-full bg-ink-900 px-12 py-5 text-lg font-semibold text-white transition-all hover:bg-ink-800 hover:shadow-2xl active:scale-95 shadow-xl"
+          {/* Hospital Portal Button */}
+          <button onClick={() => navigate('/doctor-login')}
+            className="min-w-[240px] rounded-full bg-ink-900 px-12 py-5 text-lg font-semibold text-white transition-all hover:shadow-[0_0_15px_#ae6bff] active:scale-95 shadow-xl"
           >
             Hospital Portal
           </button>
           
-          <button
-            onClick={() => navigate('/patient-login')}
-            className="min-w-[240px] rounded-full border border-ink-200 bg-white px-12 py-5 text-lg font-semibold text-ink-900 transition-all hover:bg-ink-50 hover:shadow-2xl active:scale-95 shadow-xl"
+          {/* Patient Portal Button */}
+          <button onClick={() => navigate('/patient-login')}
+            className="min-w-[240px] rounded-full border border-ink-200 bg-white px-12 py-5 text-lg font-semibold text-ink-900 transition-all hover:shadow-[0_0_15px_#ae6bff] active:scale-95 shadow-xl"
           >
             Patient Portal
           </button>
+
         </div>
       </div>
     </div>
